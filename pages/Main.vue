00b774df27115a14305f4df9330e55ff74294b58
curlyfriesplease <jmulhern85+github@gmail.com>
2024-04-21 19:48:19 +0100

    Adds logout button

--- pages/Main.vue
+++ pages/Main.vue
@@ -1,3 +1,23 @@
+<script setup>
+const user = useSupabaseUser();
+const client = useSupabaseClient();
+const router = useRouter();
+
+async function logout() {
+  try {
+    const { error } = await client.auth.signOut();
+    if (error) throw error;
+
+    router.push('/Login');
+  } catch (error) {
+    console.error('Sign out error', error);
+  }
+}
+</script>
+
 <template>
   <h1>I'm logged in</h1>
+  <button @click="logout" type="button" class="self-start btn mt-6">
+    Logout
+  </button>
 </template>
