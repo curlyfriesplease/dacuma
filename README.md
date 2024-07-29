8f8a237a930a552a3ad828e52961103dd67607ef
curlyfriesplease <jmulhern85+github@gmail.com>
2024-07-29 22:59:42 +0100

    WIP update to DB schema

--- README.md
+++ README.md
@@ -3,13 +3,14 @@
 # Dacuma
 
 A customer management app, for tracking job progress and emailing progress updates.
-*** THIS IS CURRENTLY A WIP AND NOT YET READY AS AN MVP ***
+**_ THIS IS CURRENTLY A WIP AND NOT YET READY AS AN MVP _**
 
 ## Setup
 
 1. Sign up for a Supabase DB
 2. Within Supabase, create your user from the Authentication tab
-3. A .env file is required in the project root with the following added:\
+3. Within Supabase, some initial SQL will need to be run to setup the DB schema. Copy and run the contents of the file /setupguides/supabase.sql
+4. A .env file is required in the project root with the following added:\
    SUPABASE_URL=abcdefghijklmnopqrstuv\
    SUPABASE_KEY=abcdefghijklmnopqrstuv\
    (or, if deploying, add as env vars to the platform)
---
+++ setupguides/supabase.sql
@@ -0,0 +1,53 @@
+-- Create a table for public profiles
+create table jobs (
+  id uuid references auth.users not null,
+  jobid uuid not null primary key,
+  quoteref text,
+  jobisopen boolean,
+  customername text,
+  customeremail text,
+  customerphone text,
+  itemdescription text,
+  workdescription text,
+  category text,
+  quotedprice numeric,
+  partscost numeric,
+  finalprice numeric,
+  notes text,
+  timeestimate decimal,
+  totaltime decimal,
+  flagred boolean,
+  flagyellow boolean,
+  flaggreen boolean,
+  status1timestamp timestamp with time zone,
+  status2timestamp timestamp with time zone,
+  status3timestamp timestamp with time zone,
+  status4timestamp timestamp with time zone,
+  status5timestamp timestamp with time zone,
+  status6timestamp timestamp with time zone,
+  status7timestamp timestamp with time zone,
+  updated_at timestamp with time zone,
+);
+
+create table changeslog (
+  id uuid references auth.users not null primary key,
+  changetimestamp timestamp with time zone,
+  jobid uuid references jobs,
+  status text,
+  description text
+);
+
+-- Set up Row Level Security (RLS)
+-- See https://supabase.com/docs/guides/database/postgres/row-level-security for more details.
+alter table jobs
+  enable row level security;
+create policy "Jobs are viewable only by authenticated users"
+on jobs for select
+to authenticated
+using ( true );
+
+create policy "Users can insert their own jobs." on jobs
+  for insert with check ((select auth.uid()) = id);
+
+create policy "Users can update own jobs." on jobs
+  for update using ((select auth.uid()) = id);
