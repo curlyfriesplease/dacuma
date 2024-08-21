240d5960c0dbc567cf2ae76224de0557e429c797
curlyfriesplease <jmulhern85+github@gmail.com>
2024-08-21 21:05:14 +0100

    How had I managed to include 2x <template> ?!

--- pages/Dashboard.vue
+++ pages/Dashboard.vue
@@ -9,22 +9,17 @@ import Toolbar from '../src/components/Toolbar.vue';
 
 <template>
   <Toolbar />
-
-  <div id="mainPageContentContainer">Dashboard page</div>
-
-  <template>
-    <div id="mainPageContentContainer">
-      <Joblist />
-    </div>
-  </template>
-
-  <style>
-    #mainPageContentContainer {
-      padding: 3rem 3rem 1rem 3rem;
-      background-color: rgba(0, 255, 255, 0.261);
-      width: 100%;
-      height: auto;
-      border: 2rem;
-    }
-  </style>
+  <div id="mainPageContentContainer">
+    <Joblist />
+  </div>
 </template>
+
+<style>
+#mainPageContentContainer {
+  padding: 3rem 3rem 1rem 3rem;
+  background-color: rgba(0, 255, 255, 0.261);
+  width: 100%;
+  height: auto;
+  border: 2rem;
+}
+</style>
